df36d790ccaa790f1bcfc12ba1f4b0061477aa88
jsonvi <jsonvi@gmail.com>
2011-06-07 00:14:43 +0800

    goNext() method added
    refactored navigation behavior, auto scroll when the current element is outof page

--- meerkat.js
+++ meerkat.js
@@ -14,8 +14,11 @@ var ValidKeys = function(){
         } 
     }
 };
+
+var currentPos;
+
 function doAction (func){
-    $("#MeerKatNoticeBar").text(func);
+    //$("#MeerKatNoticeBar").text(func);
 
     try {
         this[func].apply(this, Array.prototype.slice.call(arguments, 1));
@@ -31,6 +34,9 @@ function frontController(evt) {
     if (evt.ctrlKey || evt.metaKey || evt.shiftKey || evt.altKey) {
         return
     }
+    if (evt.keyCode === 9) {
+        return;
+    }
     if(target.__proto__ === HTMLInputElement.prototype ||
         target.__proto__ === HTMLTextAreaElement.prototype
     ) {
@@ -41,11 +47,74 @@ function frontController(evt) {
     } else {
         evt.preventDefault();
         evt.stopPropagation();
+        chrome.extension.sendRequest({action:"getkeys"},function(response) {
+            //alert(response.newKey);
+            var keysObj = response.keys;
+            newKey = keysObj.newKey.split("_");
+            nextKey = keysObj.nextKey.split("_");
+            var keyCodeStr = ""+ evt.keyCode;
+            switch (keyCodeStr) {
+                case newKey[1]:
+                    alert('new');
+                    break;
+                case nextKey[1]:
+                    goNext();
+                    break;
+                default:
+                    break;
+            }
+        }); 
         var keys = new ValidKeys();
         doAction(keys.getFuncName(evt.keyCode));
     }
 }
 
+var DebugMessage = '';
+function log(_str) {
+    if("undefined" !== console) {
+        console.log(_str);
+    }
+    DebugMessage = DebugMessage + " " +_str+ " ";
+}
+function goNext() {
+    if(!currentPos) {
+        currentPos = $("#feed_list li").first();
+    } else {
+        $(currentPos).removeClass("MeerKatCurrent");
+        currentPos = $(currentPos).next();
+    }
+    $(currentPos).addClass("MeerKatCurrent");
+    $(currentPos).focus();
+
+    var top = (document.documentElement.scrollTop ? 
+            document.documentElement.scrollTop :
+            document.body.scrollTop);
+
+    var coverage = window.innerHeight + top;
+    log('W:'+window.innerHeight);
+    log('UP:'+top);
+    log('WUP:'+coverage);
+
+    var elementBottom = $(currentPos).offset().top + $(currentPos).height();
+    log('ET:'+$(currentPos).offset().top);
+    log('EH:'+$(currentPos).height());
+    log('EB:'+elementBottom);
+    
+    if( coverage < elementBottom ) {
+      var scrollRange = elementBottom - coverage + top + 20;
+      log('S:'+scrollRange);
+      $('html, body').scrollTop(scrollRange);
+      //$('html, body').animate({ scrollTop: scrollRange }, 1000);
+    }
+
+    $("#MeerKatNoticeBar").text(DebugMessage);
+    DebugMessage = '';
+
+    //$('html, body').animate({ scrollTop: $(currentPos).offset().top - window.screen.availHeight/2 }, 800);
+
+
+    
+}
 function initUI() {
     var noticeBar = document.createElement("div");
     noticeBar.setAttribute("id","MeerKatNoticeBar");
