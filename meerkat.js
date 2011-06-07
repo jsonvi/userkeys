db59fffe3bdefa12485dc41a74a41203c46b421a
jsonvi <jsonvi@gmail.com>
2011-06-07 15:15:42 +0800

    goPrev() method added

--- meerkat.js
+++ meerkat.js
@@ -1,38 +1,11 @@
-var ValidKeys = function(){
-    return {
-        getFuncName:function(charCode){
-            var charStr = String.fromCharCode(charCode);
-            charStr = charStr.toUpperCase();
-            var funcPrefix = "keyPress";
-            var re = /[A-Z]/;
-            var returnStr = '';
-            if (!re.test(charStr)) {
-                return funcPrefix + charCode;
-            } else {
-                return funcPrefix + charStr;
-            }
-        } 
-    }
-};
-
 var currentPos = -1;
-
-function doAction (func){
-    //$("#MeerKatNoticeBar").text(func);
-
-    try {
-        this[func].apply(this, Array.prototype.slice.call(arguments, 1));
-    }
-    catch (e) {
-        //alert(e) // pass exception object to error handler
-    }
-}
+var DebugMessage = '';
 
 function frontController(evt) {
 
     var target = evt.target;
     if (evt.ctrlKey || evt.metaKey || evt.shiftKey || evt.altKey) {
-        return
+        return;
     }
     if (evt.keyCode === 9) {
         return;
@@ -52,6 +25,7 @@ function frontController(evt) {
             var keysObj = response.keys;
             newKey = keysObj.newKey.split("_");
             nextKey = keysObj.nextKey.split("_");
+            prevKey = keysObj.prevKey.split("_");
             var keyCodeStr = ""+ evt.keyCode;
             switch (keyCodeStr) {
                 case newKey[1]:
@@ -60,56 +34,72 @@ function frontController(evt) {
                 case nextKey[1]:
                     goNext();
                     break;
+                case prevKey[1]:
+                    goPrev();
                 default:
                     break;
             }
         }); 
-        var keys = new ValidKeys();
-        doAction(keys.getFuncName(evt.keyCode));
     }
 }
 
-var DebugMessage = '';
 function log(_str) {
     if("undefined" !== console) {
         console.log(_str);
     }
     DebugMessage = DebugMessage + " " +_str+ " ";
 }
+function goPrev() {
+    navigate(-1);
+}
 function goNext() {
+    navigate(1);
+}
+function navigate(_isForward) {
+    currentPos = currentPos + _isForward;
+    log("navPos:"+currentPos);
+    var currentObj = $("li.MIB_linedot_l:eq("+currentPos+")");
+    if($(currentObj).length>0) {
+        //$(currentObj).append("<span>{{ "+currentPos+" }}</span>");
+        var prevObj = $("li.MIB_linedot_l:eq("+(currentPos+(_isForward*-1))+")");
+        if($(prevObj).length>0) {
+            $(prevObj).removeClass("MeerKatCurrent");
+        }
+        $(currentObj).addClass("MeerKatCurrent");
+        $(currentObj).focus();
 
-    currentPos = currentPos + 1;
-    var currentObj = $(".MIB_linedot_l:eq("+currentPos+")");
-    if(currentPos > 0) {
-        var prevObj = $(".MIB_linedot_l:eq("+(currentPos-1)+")");
-        $(prevObj).removeClass("MeerKatCurrent");
-    } 
-    $(currentObj).addClass("MeerKatCurrent");
-    $(currentObj).focus();
+        var top = (document.documentElement.scrollTop ? 
+                document.documentElement.scrollTop :
+                document.body.scrollTop);
 
-    var top = (document.documentElement.scrollTop ? 
-            document.documentElement.scrollTop :
-            document.body.scrollTop);
+        var coverage = window.innerHeight + top;
+        log('W:'+window.innerHeight);
+        log('UP:'+top);
+        log('WUP:'+coverage);
 
-    var coverage = window.innerHeight + top;
-    log('W:'+window.innerHeight);
-    log('UP:'+top);
-    log('WUP:'+coverage);
+        var elementBottom = $(currentObj).offset().top + $(currentObj).height();
 
-    var elementBottom = $(currentObj).offset().top + $(currentObj).height();
-    log('ET:'+$(currentObj).offset().top);
-    log('EH:'+$(currentObj).height());
-    log('EB:'+elementBottom);
-    
-    if( coverage < elementBottom ) {
-      var scrollRange = elementBottom - coverage + top + 20;
-      log('S:'+scrollRange);
-      $('html, body').scrollTop(scrollRange);
+        log('ET:'+$(currentObj).offset().top);
+        log('EH:'+$(currentObj).height());
+        log('EB:'+elementBottom);
+
+        if( coverage < elementBottom ) {
+            var scrollRange = elementBottom - coverage + top + 20;
+            log('S:'+scrollRange);
+            $('html, body').scrollTop(scrollRange);
+        }
+        if(top > $(currentObj).offset().top) {
+            log('PrevScroll:'+$(currentObj).offset().top);
+            $('html, body').scrollTop($(currentObj).offset().top);
+        }
+
+        $("#MeerKatNoticeBar").text(DebugMessage);
+        DebugMessage = '';
+
+    } else {
+        currentPos = currentPos - _isForward;
     }
-
-    $("#MeerKatNoticeBar").text(DebugMessage);
-    DebugMessage = '';
-
+    
 }
 function initUI() {
     var noticeBar = document.createElement("div");
@@ -117,19 +107,6 @@ function initUI() {
     var postionLeft = window.innerWidth/2 - 100;
     document.body.appendChild(noticeBar);
     noticeBar.style.left = postionLeft + "px";
-}
-// event handler for key '/' (keycode = 191)
-function keyPress191() {
-    var url = window.location;
-    var re = /^http:\/\/weibo\.com\/k\//;
-    if(re.test(url)) {
-        // search result page
-        $("#sInput").focus();
-    } else {
-        // other page
-        $("#m_keyword").focus();
-    }
-    searchObj.focus();
 }
 
 document.body.addEventListener("keydown",frontController,true);
